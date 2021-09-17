d615e0ae8a01c0353930db33c4a10ea71be1660c
arielkark-versent <ariel.kark@versent.com.au>
2021-09-17 17:50:30 +1000

    init
    init2
    
    init3
    
    feat: init
    
    refactor: none
    
    feat(dev env): cionfigured TS, eslint, Husky, CZ and prettier

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
