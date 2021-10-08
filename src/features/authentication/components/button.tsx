eca331ac3b2da02ea17f130d565605db55106a34
zack686 <chenmccaig@gmail.com>
2021-10-08 21:44:46 +1100

    createdButton

---
+++ src/features/authentication/components/button.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { TouchableOpacity, Text } from 'react-native'
+
+// eslint-disable-next-line prettier/prettier
+export default function Button({
+  label,
+  onPress
+}: {
+  label: string
+  onPress: any
+}) {
+  return (
+    <TouchableOpacity
+      style={{
+        borderRadius: 8,
+        height: 50,
+        width: 245,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e94832'
+      }}
+      activeOpacity={0.7}
+      onPress={onPress}
+    >
+      <Text
+        style={{ fontSize: 18, color: 'white', textTransform: 'uppercase' }}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  )
+}
